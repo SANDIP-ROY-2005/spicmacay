d5cf2e5695fef7dabd454515e67e4eaca4d5c56e
souro-2002 <sourochatterjee92@gmail.com>
2023-07-01 11:45:27 +0530

    Navbar fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,87 +5,20 @@ import logo from "../components/images/logo.png";
 const Header = () => {
   const location = useLocation()
   return (
-    <nav className="bg-white border-gray-200 dark:bg-gray-900">
-      <div className="flex flex-wrap items-center justify-between mx-auto p-4">
-        <Link to="/" className="flex items-center">
-          <img
-            src={logo}
-            className="h-8 mr-3"
-            alt="Spicmacay"
-          />
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            Spicmacay
-          </span>
+    <header className="text-gray-600 dark:bg-slate-800 dark:text-white body-font">
+      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <Link to={'/'} className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+          <img src={logo} alt="Spicmacay" />
         </Link>
-        <button
-          data-collapse-toggle="navbar-default"
-          type="button"
-          className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-          aria-controls="navbar-default"
-          aria-expanded="false"
-        >
-          <span className="sr-only">Open main menu</span>
-          <svg
-            className="w-6 h-6"
-            aria-hidden="true"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-              clip-rule="evenodd"
-            ></path>
-          </svg>
-        </button>
-        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-            <li>
-              <Link
-                to="/"
-                className={`block py-2 pl-3 pr-4 ${location.pathname==='/' ? "text-blue-600":"dark:text-white text-black"} rounded hover:text-blue-600 md:bg-transparent md:p-0 `}
-                aria-current="page"
-              >
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/gallery"
-                className={`block py-2 pl-3 pr-4 ${location.pathname==='/gallery' ? "text-blue-600":"dark:text-white text-black"} rounded hover:text-blue-600 md:bg-transparent md:p-0 `}
-              >
-                Gallery
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/team"
-                className={`block py-2 pl-3 pr-4 ${location.pathname==='/team' ? "text-blue-600":"dark:text-white text-black"} rounded hover:text-blue-600 md:bg-transparent md:p-0 `}
-              >
-                Team
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/events"
-                className={`block py-2 pl-3 pr-4 ${location.pathname==='/events' ? "text-blue-600":"dark:text-white text-black"} rounded hover:text-blue-600 md:bg-transparent md:p-0 `}
-              >
-                Events
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/contact"
-                className={`block py-2 pl-3 pr-4 ${location.pathname==='/contact' ? "text-blue-600":"dark:text-white text-black"} rounded hover:text-blue-600 md:bg-transparent md:p-0 `}
-              >
-                Contact
-              </Link>
-            </li>
-          </ul>
-        </div>
+        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
+          <Link to={'/'} className={`mr-5 ${location.pathname==='/'?"border-b-2":""} dark:hover:text-blue-500 hover:text-gray-900`}>Home</Link>
+          <Link to={'/gallery'} className={`mr-5 ${location.pathname==='/gallery'?"border-b-2":""} dark:hover:text-blue-500 hover:text-gray-900`}>Gallery</Link>
+          <Link to={'/team'} className={`mr-5 ${location.pathname==='/team'?"border-b-2":""} dark:hover:text-blue-500 hover:text-gray-900`}>Team</Link>
+          <Link to={'/events'} className={`mr-5 ${location.pathname==='/events'?"border-b-2":""} dark:hover:text-blue-500 hover:text-gray-900`}>Events</Link>
+          <Link to={'/contact'} className={`mr-5 ${location.pathname==='/contact'?"border-b-2":""} dark:hover:text-blue-500 hover:text-gray-900`}>Contact Us</Link>
+        </nav>
       </div>
-    </nav>
+    </header>
   );
 }
 
