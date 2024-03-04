6887831bb2d33961479afc60c8e0916fb88cbc5f
Aniruddha Bandyopadhyay <111082850+aniruddh2003@users.noreply.github.com>
2024-03-04 14:42:53 +0530

    Update Events.jsx

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -10,7 +10,7 @@ const Events = () => {
       <div id="events" className='space-y-4'>
         <h1 id='event-heading' className='text-transparent text-center py-2 font-bold text-5xl'>Our Events</h1>
         <div className='grid items-center justify-center gap-y-4 p-4'>
-          <a href='https://spicmacay.vercel.app/'>
+          <a href='https://virasat-eta.vercel.app/'>
             <Card title='Virasat' image={virasat} content='Virasat is the event where we reconnect with Indian culture in a deep, enriching manner. For our audience to be absolutely bedazzled by the utmost heights of Indian classical art, music, and culture in its rich, diverse forms. Virasat is not just an event, it is a cultural experience.' />
           </a>
           <Card title='Uttarayan' image={uttarayan} content={`It's the Annual Kite festival organized by the club on the occasion of Makar Sankranti portraying the age old tradition of kite flying which truly builds an unparalleled atmosphere of joy.`} />
