f949aaa6da37b3e7bee2042efaf80c68f56ae707
Aniruddha Bandyopadhyay <111082850+aniruddh2003@users.noreply.github.com>
2024-03-04 14:41:07 +0530

    Update Events.jsx

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -10,8 +10,9 @@ const Events = () => {
       <div id="events" className='space-y-4'>
         <h1 id='event-heading' className='text-transparent text-center py-2 font-bold text-5xl'>Our Events</h1>
         <div className='grid items-center justify-center gap-y-4 p-4'>
-          <Card title='Virasat' image={virasat} content='Virasat is the event where we reconnect with Indian culture in a deep, enriching manner. For our audience to be absolutely bedazzled by the utmost heights of Indian classical art, music, and culture in its rich, diverse forms. Virasat is not just an event, it is a cultural experience.' />
-
+          <a href='https://spicmacay.vercel.app/'>
+            <Card title='Virasat' image={virasat} content='Virasat is the event where we reconnect with Indian culture in a deep, enriching manner. For our audience to be absolutely bedazzled by the utmost heights of Indian classical art, music, and culture in its rich, diverse forms. Virasat is not just an event, it is a cultural experience.' />
+          </a>
           <Card title='Uttarayan' image={uttarayan} content={`It's the Annual Kite festival organized by the club on the occasion of Makar Sankranti portraying the age old tradition of kite flying which truly builds an unparalleled atmosphere of joy.`} />
 
           <Card title='Anubhav' image={anubhav} content={`It's an Art workshop organized by us in collaboration with the BMEP for the children belonging to the deprived sections of the society with a purpose to bring a smile on their little faces and give them an opportunity to learn and rejoice`} />
