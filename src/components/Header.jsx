911d87561041692f49303938392670edc1c0efb2
Aniruddha Bandyopadhyay <111082850+aniruddh2003@users.noreply.github.com>
2024-03-04 14:37:05 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,7 +16,7 @@ const Header = ({dark}) => {
           <Link to={'/gallery'} className={`mr-5 ${location.pathname==='/gallery'?"border-b-2":""} dark:hover:text-blue-500 hover:text-gray-900`}>Gallery</Link>
           <Link to={'/team'} className={`mr-5 ${location.pathname==='/team'?"border-b-2":""} dark:hover:text-blue-500 hover:text-gray-900`}>Team</Link>
           <Link to={'/events'} className={`mr-5 ${location.pathname==='/events'?"border-b-2":""} dark:hover:text-blue-500 hover:text-gray-900`}>Events</Link>
-          <a href={'https://virasat-eta.vercel.app/'} className={`mr-5 ${location.pathname==='/events'?"border-b-2":""} dark:hover:text-blue-500 hover:text-gray-900`}>Virasat</a>
+          <a href={'https://virasat-eta.vercel.app/'} className={`mr-5 dark:hover:text-blue-500 hover:text-gray-900`}>Virasat</a>
           <Link to={'/contact'} className={`mr-5 ${location.pathname==='/contact'?"border-b-2":""} dark:hover:text-blue-500 hover:text-gray-900`}>Contact Us</Link>
         </nav>
       </div>
