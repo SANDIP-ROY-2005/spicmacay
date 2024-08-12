73823d3f3c3a252994a0b447575ed8b172570bf7
Prasun-07 <biswasprasun67@gmail.com>
2024-08-13 00:02:35 +0530

    Update wings

--- src/components/PreFinal.jsx
+++ src/components/PreFinal.jsx
@@ -4,27 +4,26 @@ import TeamImg from './TeamImg'
 function PreFinal() {
   return (
     <>
-      <TeamImg year='2022' src='aishika' name="Aishika Pal"  />
-      <TeamImg year='2022' src='ananya' name="Ananya Chatterjee"  />
-      <TeamImg year='2022' src='anubhav' name="Anubhav Mondal"  />
-      <TeamImg year='2022' src='laxmi' name="Laxmi Mondal"  />
-      <TeamImg year='2022' src='durba' name="Durba Sinha"  />
-      <TeamImg year='2022' src='ashmita' name="Ashmita Dhar"  />
-      <TeamImg year='2022' src='ishani' name="Ishani Sarkar"  />
-      <TeamImg year='2022' src='bishakha' name="Bishakha Sen"  />
-      <TeamImg year='2022' src='pratyusha' name="Pratyusha Barman"  />
-      <TeamImg year='2022' src='ojoshmi' name="Ojoshmi Dey"  />
-      <TeamImg year='2022' src='shrestha' name="Shrestha Mondal"  />
-      <TeamImg year='2022' src='ritayan' name="Ritayan Mukherjee"  />
-      <TeamImg year='2022' src='joyjit' name="Joyjit Rajbanshi"  />
-      <TeamImg year='2022' src='mithila' name="Mithila Das"  />
-      <TeamImg year='2022' src='samikshya' name="Samikshya Panda"  />
-      <TeamImg year='2022' src='abhirup' name="Abhirup Chattopadhyay"  />
-      <TeamImg year='2022' src='pratham' name="Pratham Nandy"  />
-      <TeamImg year='2022' src='rahul' name="Rahul Mondal"  />
-      <TeamImg year='2022' src='lena' name="Lena Joshy M"  />
-      <TeamImg year='2022' src='rubel' name="Rubel Gayen"  />
-      <TeamImg year='2022' src='bishal' name="Bishal Biswas"  />
+      <TeamImg year='2022' src='aishika' name="Aishika Pal" post="Art Wing"  />
+      <TeamImg year='2022' src='ananya' name="Ananya Chatterjee" post="Art & Dance Wing"  />
+      <TeamImg year='2022' src='anubhav' name="Anubhav Mondal" post="Content Wing "  />
+      <TeamImg year='2022' src='laxmi' name="Laxmi Mondal" post="Art Wing" />
+      <TeamImg year='2022' src='durba' name="Durba Sinha" post="Content Wing"  />
+      <TeamImg year='2022' src='ashmita' name="Ashmita Dhar" post="Dance Wing"  />
+      <TeamImg year='2022' src='ishani' name="Ishani Sarkar" post="Art Wing" />
+      <TeamImg year='2022' src='bishakha' name="Bishakha Sen" post="Music Wing" />
+      <TeamImg year='2022' src='pratyusha' name="Pratyusha Barman" post="Dance Wing"  />
+      <TeamImg year='2022' src='ojoshmi' name="Ojoshmi Dey" post="Dance Wing"  />
+      <TeamImg year='2022' src='shrestha' name="Shrestha Mondal" post="Content Wing" />
+      <TeamImg year='2022' src='ritayan' name="Ritayan Mukherjee" post="Music Wing" />
+      <TeamImg year='2022' src='joyjit' name="Joyjit Rajbanshi" post="Music Wing" />
+      <TeamImg year='2022' src='samikshya' name="Samikshya Panda" post="IT & Music Wing"  />
+      <TeamImg year='2022' src='abhirup' name="Abhirup Chattopadhyay" post="Music Wing"  />
+      <TeamImg year='2022' src='pratham' name="Pratham Nandy" post="IT Wing"  />
+      <TeamImg year='2022' src='rahul' name="Rahul Mondal" post="IT Wing" />
+      <TeamImg year='2022' src='lena' name="Lena Joshy M" post="Dance Wing" />
+      <TeamImg year='2022' src='rubel' name="Rubel Gayen" post="IT Wing"  />
+      <TeamImg year='2022' src='bishal' name="Bishal Biswas" post="IT Wing" />
     </>
   )
 }
